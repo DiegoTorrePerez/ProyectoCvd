e36a3a5c03a6586f49f02ef5f6edceda8a86ffa9
Dtorre <38407781+pelu29@users.noreply.github.com>
2020-04-04 14:10:44 -0500

    Se agregó la función de llamada a dos botones

--- app/src/main/java/com/test/proyectocvd/LlamadaEmergencia.java
+++ app/src/main/java/com/test/proyectocvd/LlamadaEmergencia.java
@@ -1,8 +1,14 @@
 package com.test.proyectocvd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class LlamadaEmergencia extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class LlamadaEmergencia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llamada_emergencia);
+    }
+    public void Llamada1(View view){
+        Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:113"));
+        if(ActivityCompat.checkSelfPermission(LlamadaEmergencia.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            return;
+        startActivity(i);
+    }
+    public void Llamada2(View view){
+        Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:106"));
+        if(ActivityCompat.checkSelfPermission(LlamadaEmergencia.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            return;
+        startActivity(i);
     }
 }
